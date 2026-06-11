4abf092043a11c57f1fef785cfb5873d483c14cc
linyi2134 <op0610315019@163.com>
2026-06-11 16:06:56 +0800

    Delete chess.cpp

--- chess.cpp
+++
@@ -1,9 +0,0 @@
-#include "chess.h"
-
-
-Chess::Chess(int row, int col, const std::string& code, int ownerId)
-    : row(row), col(col), code(code), owner(ownerId), alive(true)
-{
-}
-
-Chess::~Chess() = default;
